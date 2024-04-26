7792ce7059d9fb8b569c1256c918b42582bc0970
devCarlosSantoss <carlosvinicius357890@gmail.com>
2024-04-26 14:52:06 -0300

    Componentização do estilo

--- src/components/LastReleases/LastReleases.jsx
+++ src/components/LastReleases/LastReleases.jsx
@@ -1,7 +1,33 @@
+import { books } from './DataLastReleases.js'
+import styled from 'styled-components'
+import { Titulo } from '../Title/Title.js'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
 const LastReleases = () => {
     return (
         <>
-        <h1>Ultimos Lançamentos</h1>
+        <UltimosLancamentosContainer>
+            <Titulo cor='#EB9B00'>Últimos Lançamentos</Titulo>
+            <NovosLivrosContainer>
+                {books.map((livros) => {
+                    return <img src={livros.src} alt={livros.nome} id={livros.id}/>
+                })}
+            </NovosLivrosContainer>
+        </UltimosLancamentosContainer>
         </>
     )
 }
